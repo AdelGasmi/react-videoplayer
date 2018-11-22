cb65ac1f8b07eddf0202dff05da6de3df6ffeafe
Adel <ea_gasmi@esi.dz>
2018-11-23 00:28:11 +0100

    commiting code
    Here is the pushed code - first version

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetails from './components/video_details';
+
+const API_KEY = 'AIzaSyBm2Fym1drtEii5tEKO1wMacmeCAiYmzpg';
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({ key: API_KEY, term: 'Yes Theory' }, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetails video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={
+                        selectedVideo => this.setState({ selectedVideo })
+                    }
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
